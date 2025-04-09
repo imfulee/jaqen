37f32bcec1ffab702dacc0148841584eaa22bdb9
imfulee <freems5192@gmail.com>
2025-04-09 23:29:47 +0800

    feat: steam app id mapping

--- internal/constants.go
+++ internal/constants.go
@@ -1,11 +1,21 @@
 package internal
 
+import mapper "jaqen/pkgs"
+
 const (
 	DefaultPreserve       = false
 	DefaultXMLPath        = "./config.xml"
 	DefaultRTFPath        = "./newgen.rtf"
 	DefaultImagesPath     = "./"
-	DefaultFMVersion      = "2024"
+	DefaultFMVersion      = mapper.FMVersion2024
 	DefaultConfigPath     = "./jaqen.toml"
 	DefaultAllowDuplicate = false
 )
+
+var SteamAppIdMap = map[string]string{
+	mapper.FMVersion2020: "1100600",
+	mapper.FMVersion2021: "1263850",
+	mapper.FMVersion2022: "1569040",
+	mapper.FMVersion2023: "1904540",
+	mapper.FMVersion2024: "2252570",
+}
